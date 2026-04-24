caf8d4c9b7431702f16b495a7ad05f8ea13f221e
Mwirigi <mwirigimburugu81@gmail.com>
2026-04-24 14:55:05 +0300

    feat: add CLI command to bootstrap first user

---
+++ src/Command/CreateUserCommand.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:create-user',
+    description: 'Creates a new user.',
+)]
+class CreateUserCommand extends Command
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private UserPasswordHasherInterface $passwordHasher
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('email', InputArgument::REQUIRED, 'The email of the user')
+            ->addArgument('password', InputArgument::REQUIRED, 'The plain password')
+            ->addArgument('fullname', InputArgument::REQUIRED, 'The full name of the user')
+            ->addArgument('nationalId', InputArgument::REQUIRED, 'The National ID of the user')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $email = $input->getArgument('email');
+        $password = $input->getArgument('password');
+        $fullname = $input->getArgument('fullname');
+        $nationalId = $input->getArgument('nationalId');
+
+        $user = new User();
+        $user->setEmail($email);
+        $user->setFullName($fullname);
+        $user->setNationalId($nationalId);
+        $user->setDateOfBirth(new \DateTime('1990-01-01')); // Default dob
+        $user->setRoles(['ROLE_ADMIN', 'ROLE_USER']);
+
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $password);
+        $user->setPassword($hashedPassword);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $io->success(sprintf('User %s successfully created.', $email));
+
+        return Command::SUCCESS;
+    }
+}
